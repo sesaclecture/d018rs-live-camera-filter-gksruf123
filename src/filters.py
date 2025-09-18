f927c1ef81b0f21bd5aa22db0569ab7974ddad97
gksruf1 <duagksruf1@gmail.com>
2025-09-18 17:18:23 +0900

    camera_filter

--- src/filters.py
+++ src/filters.py
@@ -5,24 +5,44 @@ import numpy as np
 class Filters:
     # TODO: Image kernels
     Kernels = {
+        "original": np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=np.float32),
+        "blur": np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32)/9,
+        "gaussian blur": np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.float32)/16,
+        "sharpen": np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32),
+        "sobel (x)": np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32),
+        "sobel (y)": np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32),
+        "edge detection": np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32),
+        "emboss": np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]], dtype=np.float32)
     }
 
     def __init__(self, kernels=Kernels):
         self.kernels = kernels
         # TODO: Implement internal variables
+        self.names = list(self.kernels.keys())
+        self.idx = 0
 
     def apply_filter(self, frame, filter_name) -> np.array:
         # TODO: Apply the selected filter kernel to the frame
-        pass
+        self.filter_name = filter_name
+        name = filter_name.lower()
+        kernel = self.kernels.get(name)
+        if kernel is None:
+            return frame
+        out = cv2.filter2D(frame, ddepth=-1, kernel=kernel, borderType=cv2.BORDER_DEFAULT)
+        return out
 
     def get_current_filter_name(self) -> str:
         # TODO: Return currently set kernels's name
-        pass
+        return self.names[self.idx]
 
     def switch_next_filter(self):
         # TODO: Update currently selected kernel to the next
-        pass
+        self.idx += 1
+        if self.idx >= len(self.names):
+            self.idx = 0
 
     def switch_previous_filter(self):
         # TODO: Update currently selected kernel to the previous
-        pass
+        self.idx -= 1
+        if self.idx <= 0:
+            self.idx = len(self.names) - 1
